3107e824ee235ca113f18e3f193378d0b64eb671
Fahmi XD <fahmixd404@gmail.com>
2025-01-10 01:28:24 +0000

    first commit

--- proto-extract/generate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd $(dirname $0)
-set -euo pipefail
-if [[ ! -f "protos.js" ]]; then
-	echo "Please download the WhatsApp JavaScript modules with protobuf schemas into protos.js first"
-	exit 1
-fi
-node index.js
-node generateStatics.js
